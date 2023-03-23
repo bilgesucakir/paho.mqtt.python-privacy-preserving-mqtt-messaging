3089486c97b8ba01488e09778f0dc6ce19794394
Bilgesu Çakır <95298678+bilgesucakir@users.noreply.github.com>
2023-03-23 19:05:50 +0300

    class added
    methods are not finished yet

---
+++ src/paho/mqtt/brokerconnection.py
@@ -0,0 +1,6 @@
+class BrokerConnection:
+
+    __slots__ = ("session_key", "pub_key", "priv_key", "session_key") #priv and pub keyi burada tutmaya gerek olmayabilir, burada classa bile gerek olmayabilir
+
+    def __init__(self) -> None:
+        pass
